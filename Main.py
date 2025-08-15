69b115635a43e02465d8a0810cc80e400ae583a7
Sidd <siddarthnair29@gmail.com>
2025-08-15 14:59:36 +0000

    ye

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
-AAAAAAAAAAAAAAAAAAAAA
+import mysql.connector as mys
+mycon = mys.connect(host = 'localhost', user = 'root', passwd = 'LoremIpsum')
+c = mycon.cursor()
+
+c.execute("create database SK")
+c.execute("use SK")
+c.execute("create table Pantry (ItemNo integer primary key, Name varchar(30), Qty integer, Calorie integer, Expiry Date)")
+c.execute("create table Recipes (RecipeNo integer primary key, Name varchar(30), Ingredients varchar(200), NetCalorie integer)")
+mycon.commit()
